8388c96edfec7b8738e5a1262823c7a02148008e
simone pozzobon <info@simonepozzobon.com>
2017-10-16 11:24:06 +0200

    Vue Router & Vue Resource

--- themes/default/js/home.js
+++ themes/default/js/home.js
@@ -1,19 +1,19 @@
-webpackJsonp([1],{
+webpackJsonp([2],{
 
-/***/ 8:
+/***/ 28:
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(9);
+module.exports = __webpack_require__(29);
 
 
 /***/ }),
 
-/***/ 9:
-/***/ (function(module, exports, __webpack_require__) {
+/***/ 29:
+/***/ (function(module, exports) {
 
 "use strict";
-
+throw new Error("Module build failed: Error: ENOENT: no such file or directory, open '/Users/simonepozzobon/isabella/resources/assets/js/home.js'");
 
 /***/ })
 
-},[8]);
+},[28]);
